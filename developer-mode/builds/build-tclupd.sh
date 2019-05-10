3d9ca3ed83b6cbcc8ba93c8a8aa6caaa96b6d5f2
snoe925 <snoe925@gmail.com>
2019-05-10 17:25:56 -0500

    Create build-tclupd.sh

---
+++ developer-mode/builds/build-tclupd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /work/tcpudp ]; then
+	cd /work && sh /builds/download-tcludp.sh
+fi
+
+cd /work/tcludp || exit 1
+autoreconf --force --install --verbose
+configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include
+make && make install
+
+build_cleanup
