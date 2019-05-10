dbe41a252d013cc606997fa48eac3ca4145fcc86
snoe925 <snoe925@gmail.com>
2019-05-10 17:51:36 -0500

    Update build-zookeepertcl.sh

--- developer-mode/builds/build-zookeepertcl.sh
+++ developer-mode/builds/build-zookeepertcl.sh
@@ -3,13 +3,13 @@
 
 build_setup
 
-if [ ! -d /work/Pgtcl ]; then
+if [ ! -d /work/zookeepertcl ]; then
 	cd /work && sh /builds/download-zookeepertcl.sh
 fi
 
 cd /work/zookeepertcl || exit 1
 autoreconf --force --install --verbose
-configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include
+./configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include
 make && make install
 
 build_cleanup
