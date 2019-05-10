f3b27e36c0183064c7d21c9a9233f82a0d61982b
snoe925 <snoe925@gmail.com>
2019-05-10 17:24:03 -0500

    Create build-tclzlib.sh

---
+++ developer-mode/builds/build-tclzlib.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /work/Pgtcl ]; then
+	cd /work && sh /builds/download-tclzlib.sh
+fi
+
+cd /work/tclzlib || exit 1
+autoreconf --force --install --verbose
+configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include
+make && make install
+
+build_cleanup
