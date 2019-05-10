b2d43accb9ba7c04b2f099f57b37d822adeca2fb
snoe925 <snoe925@gmail.com>
2019-05-10 14:10:44 -0500

    Create build-pgtcl.sh

---
+++ developer-mode/builds/build-pgtcl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /work/Pgtcl ]; then
+	cd /work && sh /builds/download-pgtcl.sh
+fi
+
+cd /work/Pgtcl || exit 1
+autoreconf --force --install --verbose
+configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include --with-postgres-include=/usr/include/postgresql --with-postgres-lib=/usr/lib/x86_64-linux-gnu
+make && make install
+
+build_cleanup
