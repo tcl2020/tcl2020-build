f701871f5fbeb2a6f4e3f96e3094a3bd273d0c48
Shannon Noe <shannon.noe@flightaware.com>
2019-05-22 15:29:05 -0500

    Fix TCL version

--- developer-mode/builds/build-cpptcl.sh
+++ developer-mode/builds/build-cpptcl.sh
@@ -7,7 +7,6 @@ if [ ! -d /workspace/cpptcl ]; then
     cd /workspace && sh /builds/download-cpptcl.sh
 fi
 
-export TCL_VERSION_MINOR=7
 cd /workspace/cpptcl && make clean && make && make install
 
 build_cleanup
