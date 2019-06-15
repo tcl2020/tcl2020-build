109d769786286c76657d755c3f4441eb7847589d
Shannon Noe <shannon.noe@flightaware.com>
2019-06-15 18:58:06 -0500

    Add skanet s6

---
+++ developer-mode/builds/build-s6.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /workspaces/s6 ]; then
+	mkdir -p /workspaces/s6
+	cd /workspaces/s6 && sh /builds/download-s6.sh
+fi
+
+cd /workspaces/s6 || exit 1
+
+cd skalibs && ./configure && make && make install && \
+cd ../execline && ./configure && make && make install && \
+cd ../s6 && ./configure && make && make install
+
+build_cleanup
---
+++ developer-mode/builds/download-s6.sh
@@ -0,0 +1,3 @@
+git clone -b v2.6.4.0 https://github.com/skarnet/skalibs.git && \
+git clone -b v2.7.1.1 https://github.com/skarnet/s6.git && \
+git clone -b v2.5.0.0 https://github.com/skarnet/execline.git
