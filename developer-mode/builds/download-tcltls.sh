7a7bc3c612ab5a24511be5a42adca3491135112c
Shannon Noe <shannon.noe@flightaware.com>
2019-07-01 20:51:02 +0000

    Upgrade and follow server redirects

--- developer-mode/builds/download-tcltls.sh
+++ developer-mode/builds/download-tcltls.sh
@@ -1,2 +1,2 @@
-mkdir -p tcltls && cd tcltls && curl 'https://core.tcl.tk/tcltls/uv/tcltls-1.7.16.tar.gz' | tar -xvzf -
+mkdir -p tcltls && cd tcltls && curl -L 'https://core.tcl.tk/tcltls/uv/tcltls-1.7.18.tar.gz' | tar -xvzf -
 
