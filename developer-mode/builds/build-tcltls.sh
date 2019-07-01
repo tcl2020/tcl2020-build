06ea74cd2e4add2d7daf2fc9d3277aa27ccb34e1
Shannon Noe <shannon.noe@flightaware.com>
2019-07-01 20:53:54 +0000

    Remove unneed curl call

--- developer-mode/builds/build-tcltls.sh
+++ developer-mode/builds/build-tcltls.sh
@@ -8,8 +8,7 @@ if [ ! -d /workspaces/tcltls ]; then
 fi
 
 cd /workspaces/tcltls || exit 1
-curl 'https://core.tcl.tk/tcltls/uv/tcltls-1.7.16.tar.gz' | tar -xvzf -
-cd tcltls-1.7.16 && ./configure --with-openssl-dir=/usr/lib/ssl && make && make install
+cd tcltls-1.7.18 && ./configure --with-openssl-dir=/usr/lib/ssl && make && make install
 
 build_cleanup
 
