29cab5484db0d90e43546007d6ba30b640ba4e4c
Shannon Noe <shannon.noe@flightaware.com>
2019-07-02 15:15:24 -0500

    Add memcache-for-Tcl

---
+++ developer-mode/builds/build-memcached-for-Tcl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /workspaces/memcached-for-Tcl ]; then
+    cd /workspaces && sh /builds/download-memcached-for-Tcl.sh
+fi
+
+cd /workspaces/memcached-for-Tcl && \
+    autoreconf -vi && \
+    ./configure && make && make install
+
+build_cleanup
+
---
+++ developer-mode/builds/download-memcached-for-Tcl.sh
@@ -0,0 +1,2 @@
+git clone -b v1.2.1 https://github.com/bovine/memcached-for-Tcl.git
+
