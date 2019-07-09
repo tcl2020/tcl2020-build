6ff9f631d9264cd0e40a630d1352c93349bdd019
Shannon Noe <shannon.noe@flightaware.com>
2019-07-09 08:48:27 -0500

    Add TclXML

---
+++ developer-mode/builds/build-tclxml.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /workspaces/TclXML ]; then
+	cd /workspaces && sh /builds/download-tclxml.sh
+fi
+
+cd /workspaces/TclXML || exit 1
+autoreconf --force --install --verbose
+./configure  --with-tcl=/usr/lib --with-tclinclude=/usr/include
+make && make install
+
+build_cleanup
---
+++ developer-mode/builds/download-tclxml.sh
@@ -0,0 +1,1 @@
+git clone -b v3.3 https://github.com/flightaware/TclXML.git
