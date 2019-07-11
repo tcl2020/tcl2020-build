736e3b8509d6b174e54ee979d9e7dc3764ff6fd3
Shannon Noe <shannon.noe@flightaware.com>
2019-07-11 14:19:20 -0500

    Add apache2 upgrade g++-8

--- developer-mode/setup-docker-env.sh
+++ developer-mode/setup-docker-env.sh
@@ -1,15 +1,15 @@
 apt-get update -qq
 
-apt-get install -y git gcc-7 g++-7 make curl automake autoconf \
+apt-get install -y git gcc-8 g++-8 make curl automake autoconf \
     minizip vim libreadline-dev libtool cmake libyajl-dev \
     libssl-dev openssl libpq-dev libzookeeper-mt-dev \
     libboost-all-dev librdkafka-dev libuv1-dev swig3.0 \
     libgd-dev pkg-config gdb libcurl4-openssl-dev \
-    libmemcached-dev libxml2-dev libxslt-dev
+    libmemcached-dev libxml2-dev libxslt-dev apache2-dev
 
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-update-alternatives --set cc /usr/bin/gcc-7
-update-alternatives --set c++ /usr/bin/g++-7
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+update-alternatives --set cc /usr/bin/gcc-8
+update-alternatives --set c++ /usr/bin/g++-8
 
 dpkg-query -l 2>&1 | sed "s,^,package-selections: ,"
 
