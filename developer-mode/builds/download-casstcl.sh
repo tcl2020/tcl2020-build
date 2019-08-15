c44ef2974fe4618d746440863476d41c48727039
Jonathan Cone <jonathan.cone@flightaware.com>
2019-08-15 13:15:08 -0500

    Remove extraneous v in cpp-driver version

--- developer-mode/builds/download-casstcl.sh
+++ developer-mode/builds/download-casstcl.sh
@@ -1,3 +1,3 @@
-git clone -b v2.12.0 https://github.com/datastax/cpp-driver.git
+git clone -b 2.13.0 https://github.com/datastax/cpp-driver.git
 git clone -b v2.13.0 https://github.com/flightaware/casstcl.git
 
