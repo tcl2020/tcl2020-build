741bf54dd899dc808d0c194d58852dc0f6fe138c
Jonathan Cone <conej730@users.noreply.github.com>
2019-08-14 21:36:21 -0500

    Fix build-libspatialite

--- developer-mode/builds/build-libspatialite.sh
+++ developer-mode/builds/build-libspatialite.sh
@@ -7,7 +7,15 @@ if [ ! -d  /workspaces/libspatialite ]; then
 	cd /workspaces && sh /builds/download-libspatialite.sh
 fi
 
-cd /workspaces/libspatialite || exit 1
+echo Build libspatialite
+BUILD_VERSION=$(cd /workspaces/libspatialite; ls -v | tail -1)
+if [ ! -d "/workspaces/libspatialite/$BUILD_VERSION" ]; then
+    echo "Cannot determine libspatialite build version"
+    exit 1
+else
+    echo "Building /workspaces/libspatialite/$BUILD_VERSION"
+fi
+cd "/workspaces/libspatialite/$BUILD_VERSION/" || exit 1
 autoreconf -vi
 ./configure --prefix=/usr --exec-prefix=/usr --disable-freexl
 make && make install
