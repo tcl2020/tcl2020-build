1ceac0c1c80633008eb804d816c49d689ca76c4e
Jonathan Cone <jonathan.cone@flightaware.com>
2019-08-15 21:55:28 -0500

    Update tcltls build to extract version

--- developer-mode/builds/build-tcltls.sh
+++ developer-mode/builds/build-tcltls.sh
@@ -7,8 +7,16 @@ if [ ! -d /workspaces/tcltls ]; then
 	cd /workspaces && sh /builds/download-tcltls.sh
 fi
 
-cd /workspaces/tcltls || exit 1
-cd tcltls-1.7.18 && ./configure --with-openssl-dir=/usr/lib/ssl && make && make install
+echo Build tcltls
+BUILD_VERSION=$(cd /workspaces/tcltls; ls -v | tail -1)
+if [ ! -d "/workspaces/tcltls/$BUILD_VERSION" ]; then
+	echo "Cannot determine tcltls build version"
+	exit 1
+else
+	echo "Building /workspaces/tcltls/$BUILD_VERSION"
+fi
+cd "/workspaces/tcltls/$BUILD_VERSION/" || exit 1
+./configure --with-openssl-dir=/usr/lib/ssl && make && make install
 
 build_cleanup
 
