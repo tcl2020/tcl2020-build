911f02f60526b55d203e9b212abedca56e1132f1
Jonathan Cone <conej730@users.noreply.github.com>
2019-09-09 12:49:34 -0500

    Add tcllibc build step (#9)
    * Install tcllibc
    
    * Add build tcllibc step
    
    * Bump zookeepertcl to v1.1.1

---
+++ developer-mode/builds/build-tcllibc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+. /builds/common.sh
+
+build_setup
+
+if [ ! -d /workspaces/tcllib ]; then
+	cd /workspaces && sh /builds/download-tcllib.sh
+fi
+
+cd /workspaces/tcllib
+# Build the critcl components of tcllib
+/usr/bin/tclsh8.6 sak.tcl critcl
+cp -r modules/tcllibc /usr/lib/tcllibc
+
+build_cleanup
+
--- developer-mode/builds/download-zookeepertcl.sh
+++ developer-mode/builds/download-zookeepertcl.sh
@@ -1,1 +1,1 @@
-git clone -b v1.1.0 https://github.com/flightaware/zookeepertcl.git
+git clone -b v1.1.1 https://github.com/flightaware/zookeepertcl.git
