f7074a46de15e469e37f35354d42c681fddfa492
snoe925 <snoe925@gmail.com>
2019-10-28 10:46:08 -0500

    Add libbsd-dev for setproctitle

--- developer-mode/setup-docker-env.sh
+++ developer-mode/setup-docker-env.sh
@@ -6,7 +6,7 @@ apt-get install -y git gcc-8 g++-8 make curl automake autoconf \
     libboost-all-dev librdkafka-dev libuv1-dev swig3.0 \
     libgd-dev pkg-config gdb libcurl4-openssl-dev \
     libmemcached-dev libxml2-dev libxslt-dev apache2-dev apache2 \
-    libgeos-dev libproj-dev
+    libgeos-dev libproj-dev libbsd-dev
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 update-alternatives --set cc /usr/bin/gcc-8
