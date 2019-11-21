ddd231465695a99724b1d899dfc8c0c35828ecbd
snoe925 <snoe925@gmail.com>
2019-11-21 11:05:14 -0600

    Upgrade version and change to tcl-lang.org

--- developer-mode/builds/download-tcltls.sh
+++ developer-mode/builds/download-tcltls.sh
@@ -1,2 +1,2 @@
-mkdir -p tcltls && cd tcltls && curl -L 'https://core.tcl.tk/tcltls/uv/tcltls-1.7.18.tar.gz' | tar -xvzf -
+mkdir -p tcltls && cd tcltls && curl -L 'https://core.tcl-lang.org/tcltls/uv/tcltls-1.7.20.tar.gz' | tar -xvzf -
 
