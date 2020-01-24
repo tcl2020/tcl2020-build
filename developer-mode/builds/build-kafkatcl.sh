008e775bc6f1c06803acf40b0b0b8296ad517ba4
Peter da Silva <peter.dasilva@flightaware.com>
2020-01-24 13:13:22 +0000

    Fix rdkafka build.

--- developer-mode/builds/build-kafkatcl.sh
+++ developer-mode/builds/build-kafkatcl.sh
@@ -8,8 +8,7 @@ if [ ! -d /workspaces/kafkatcl ]; then
 fi
 
 cd /workspaces/librdkafka || exit 1
-./configure --with-tcl=/usr/lib --with-tclinclude=/usr/include
-make && make install && ldconfig
+./configure && make && make install && ldconfig
 
 cd /workspaces/kafkatcl || exit 1
 autoreconf --force --install --verbose
