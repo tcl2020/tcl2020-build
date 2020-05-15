c7d4876ed293e9a1eaef520fbbe4cd1e6a367b66
Jonathan Cone <jonathan.cone@flightaware.com>
2020-05-15 17:15:40 +0000

    Use pip to install cmake (currently 3.17)
        Debian only has cmake 3.13 and there are some nice features in later
        versions of cmake.  Pip now installs cmake 3.17 so install pip3 and
        use it to install cmake

--- developer-mode/setup-docker-env.sh
+++ developer-mode/setup-docker-env.sh
@@ -1,12 +1,14 @@
 apt-get update -qq
 
 apt-get install -y git gcc-8 g++-8 make curl automake autoconf \
-    minizip vim libreadline-dev libtool cmake libyajl-dev \
+    minizip vim libreadline-dev libtool python3-pip libyajl-dev \
     libssl-dev openssl libpq-dev libzookeeper-mt-dev \
     libboost-all-dev libuv1-dev swig3.0 \
     libgd-dev pkg-config gdb libcurl4-openssl-dev \
     libmemcached-dev libxml2-dev libxslt-dev apache2-dev apache2 \
     libgeos-dev libproj-dev libbsd-dev
+
+pip3 install cmake
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 update-alternatives --set cc /usr/bin/gcc-8
