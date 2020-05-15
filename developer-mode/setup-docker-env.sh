33e6d5b54c033f4fc3e33c50f81597dd4393ee21
Jonathan Cone <conej730@users.noreply.github.com>
2020-05-15 12:39:06 -0500

    Tag the cmake version that pip installs
    Co-authored-by: Chris Roberts <NasaGeek@users.noreply.github.com>

--- developer-mode/setup-docker-env.sh
+++ developer-mode/setup-docker-env.sh
@@ -8,11 +8,10 @@ apt-get install -y git gcc-8 g++-8 make curl automake autoconf \
     libmemcached-dev libxml2-dev libxslt-dev apache2-dev apache2 \
     libgeos-dev libproj-dev libbsd-dev
 
-pip3 install cmake
+pip3 install "cmake==3.17.2"
 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 update-alternatives --set cc /usr/bin/gcc-8
 update-alternatives --set c++ /usr/bin/g++-8
 
 dpkg-query -l 2>&1 | sed "s,^,package-selections: ,"
-
