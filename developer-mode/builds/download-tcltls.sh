9a92c0f1ec474a6c8ab2c6e73713e89b0f9ec34e
Peter da Silva <peter.dasilva@flightaware.com>
2020-10-13 14:07:26 +0000

    Update tcltls to 1.7.22 for ssl version

--- developer-mode/builds/download-tcltls.sh
+++ developer-mode/builds/download-tcltls.sh
@@ -1,2 +1,2 @@
-mkdir -p tcltls && cd tcltls && curl -L 'https://core.tcl-lang.org/tcltls/uv/tcltls-1.7.20.tar.gz' | tar -xvzf -
+mkdir -p tcltls && cd tcltls && curl -L 'https://core.tcl-lang.org/tcltls/uv/tcltls-1.7.22.tar.gz' | tar -xvzf -
 
