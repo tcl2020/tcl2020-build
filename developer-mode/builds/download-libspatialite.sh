6a0731a5e7bf11c98751288bed2664d217cc8fa3
Peter da Silva <peter.dasilva@flightaware.com>
2021-02-05 22:24:13 +0000

    Location of libspatialite directory seems to have changed.

--- developer-mode/builds/download-libspatialite.sh
+++ developer-mode/builds/download-libspatialite.sh
@@ -1,1 +1,1 @@
-rm -rf libspatialite && mkdir libspatialite && cd libspatialite && curl http://www.gaia-gis.it/gaia-sins/libspatialite-4.3.0a.tar.gz | tar -xzf -
+rm -rf libspatialite && mkdir libspatialite && cd libspatialite && curl http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.3.0a.tar.gz | tar -xzf -
