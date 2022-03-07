2d006dfb589d378fad04b60d719e096c89892870
Peter da Silva <peter.dasilva@flightaware.com>
2022-03-07 16:17:04 +0000

    Update sqlite to 3.38.0

--- developer-mode/builds/download-sqlite3.sh
+++ developer-mode/builds/download-sqlite3.sh
@@ -1,1 +1,1 @@
-rm -rf sqlite && mkdir sqlite && cd sqlite && curl https://www.sqlite.org/2019/sqlite-autoconf-3290000.tar.gz | tar -xzf -
+rm -rf sqlite && mkdir sqlite && cd sqlite && curl https://www.sqlite.org/2022/sqlite-autoconf-3380000.tar.gz | tar -xzf -
