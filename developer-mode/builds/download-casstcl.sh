4fe50f53de2e0169f73b4a9b597cfb5bf5465851
Peter da Silva <peter.dasilva@flightaware.com>
2022-06-08 13:16:13 +0000

    Update to latest cpp-driver and casstcl

--- developer-mode/builds/download-casstcl.sh
+++ developer-mode/builds/download-casstcl.sh
@@ -1,3 +1,3 @@
-git clone -b 2.15.0 https://github.com/datastax/cpp-driver.git
-git clone -b v2.16.0 https://github.com/flightaware/casstcl.git
+git clone -b 2.16.2 https://github.com/datastax/cpp-driver.git
+git clone -b v2.16.1 https://github.com/flightaware/casstcl.git
 
