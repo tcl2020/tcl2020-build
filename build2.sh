efcdf5619fda1620bf299d1381df3c5ec7efb269
Peter da Silva <peter.dasilva@flightaware.com>
2022-06-21 14:39:07 +0000

    Add some error handling for workspace cleanup

--- build2.sh
+++ build2.sh
@@ -3,13 +3,15 @@
 # Use the dev docker image to run a complete build. Loosen security for easier debugging.
 
 # Clean up from previous builds
-rm -rf $PWD/workspaces
+if [ -d workspaces ]
+then rm -rf $PWD/workspaces || echo "You've probably got some root-owned trash in workspaces" && exit
+fi
 mkdir -p $PWD/workspaces
 
 # Run with debugging. Maybe add -v $HOME:$HOME for access to your own cloned repos.
 docker run -d \
 	-v $PWD/workspaces:/workspaces -v $PWD/developer-mode/builds:/builds \
-	-cap-add=SYS_PTRACE --security-opt=apparmor:unconfined --security-opt=seccomp:unconfined \
+	--cap-add=SYS_PTRACE --security-opt=apparmor:unconfined --security-opt=seccomp:unconfined \
 	--name dev tcl-2020-dev
 
 # Kick off build
