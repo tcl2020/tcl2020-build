39f807f2abe7b0d127966b041046effa7528b98b
Jeff Lawson <jeff.lawson@flightaware.com>
2025-01-07 22:48:27 +0000

    fix: special handling for git submodules

--- developer-mode/builds/download-tclclockmod.sh
+++ developer-mode/builds/download-tclclockmod.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/sebres/tclclockmod.git && cd tclclockmod && git checkout f4e8bfffc981e9afa6495927788b85ba7e1341a9
+git clone https://github.com/sebres/tclclockmod.git && cd tclclockmod && git checkout f4e8bfffc981e9afa6495927788b85ba7e1341a9 && git submodule init && git submodule update
